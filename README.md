f4154b16aab0d1badbf90ff4b21efbe270de63d6
qinhan <qinhan@xingshulin.com>
2016-02-14 16:27:30 +0800

    init project

---
+++ README.md
@@ -0,0 +1,1 @@
+http://thunder-project.org/
---
+++ start.sh
@@ -0,0 +1,7 @@
+export SPARK_HOME=/Users/twer/Development/Tools/spark-1.6.0-bin-hadoop2.6
+echo $SPARK_HOME
+
+export IPYTHON=1
+export IPYTHON_OPTS="notebook"
+
+thunder
